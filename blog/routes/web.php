f95c9768da77c9436032d2ad80b43874dbe0eb54
Dariela Vazquez <darielavzz@gmail.com>
2017-06-15 23:08:48 -0500

    Tutorial 6

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,14 +11,19 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/tasks', function () {
 
-    $tasks = [
-        'Go to the store',
-        'Finish my screencast',
-        'Clean the house',
+    $tasks = DB::table('tasks')->latest()->get();
 
-    ];
-
-    return view('welcome' , compact('tasks'));
+    return view('tasks/index' , compact('tasks'));
 });
+
+Route::get('/tasks/{task}', function($id) {
+
+
+    $task = DB::table('tasks')->find($id);
+
+
+    return view('tasks/show', compact('task'));
+});
+
