d6647851486f6e8fc61eb40d020c347e92dac7d8
Dariela Vazquez <darielavzz@gmail.com>
2017-06-15 21:26:51 -0500

    Tutorial 5

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $tasks = [
+        'Go to the store',
+        'Finish my screencast',
+        'Clean the house',
+
+    ];
+
+    return view('welcome' , compact('tasks'));
 });
